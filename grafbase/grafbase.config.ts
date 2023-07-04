1b179e34193fbe76e17b100c6851f5e6cc790f2f
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-04 15:38:42 +0200

    add user schema

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,14 @@
 import { g, auth, config } from '@grafbase/sdk'
 
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(),
+})
 export default config({
   schema: g
 
