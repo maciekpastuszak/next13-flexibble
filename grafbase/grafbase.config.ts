9981af3908d428b2fc93cae9ddc6ab0a33142e02
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-04 15:42:27 +0200

    add grafbase model

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -4,11 +4,23 @@ const User = g.model('User', {
   name: g.string().length({ min: 2, max: 20 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string(),
+  description: g.string().optional(),
   githubUrl: g.url().optional(),
   linkedInUrl: g.url().optional(),
   projects: g.relation(),
 })
+
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3}),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User)
+
+})
+
 export default config({
   schema: g
 
