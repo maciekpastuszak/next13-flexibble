548e1ee651dab21218d328bc4f2518d175b5947a
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-05 11:51:39 +0200

    add import and state to AuthProviders

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+"use client";
+
+import { getProviders, signIn } from 'next-auth/react';
+import { useState, useEffect } from "react";
 
 const AuthProviders = () => {
+
+  const [providers, setProviders] = useState(null)
+
   return (
     <div>AuthProviders</div>
   )
