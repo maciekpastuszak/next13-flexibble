bb201d7d73176c30958eca00a3cd2b5e686fc5e7
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-05 11:54:35 +0200

    typo

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -3,18 +3,18 @@
 import { getProviders, signIn } from 'next-auth/react';
 import { useState, useEffect } from "react";
 
-type Providers = {
+type Provider = {
   id: string;
   name: string;
   type: string;
   signinUrl: string;
   callbackUrl: string;
-  signinUrlParams: Record<string, string | undefined>
+  signinUrlParams?: Record<string, string | undefined>
 }
 
 const AuthProviders = () => {
 
-  const [providers, setProviders] = useState<Providers | null>(null);
+  const [providers, setProviders] = useState<Provider | null>(null);
 
   return (
     <div>AuthProviders</div>
